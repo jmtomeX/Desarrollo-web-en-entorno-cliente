d52c2642a0243ff799ae0ac5ce4ae24f948ad34a
jmtomeX <jmtomeX@gmail.com>
2019-10-22 14:50:40 +0200

    Modificación con errores, no elimina todas las clases a la vez

--- ejer_galeria_carrera/assets/js/main.js
+++ ejer_galeria_carrera/assets/js/main.js
@@ -2,7 +2,7 @@
 function cargarImg() {
   var fotosArray = [
     "img_1.jpg",
-    "img_8.jpg",
+    "img_2.jpg",
     "img_3.jpg",
     "img_4.jpg",
     "img_5.jpg",
@@ -12,23 +12,30 @@ function cargarImg() {
     "img_9.jpg"
   ];
   var cont = 0;
-  document.write("<div class = 'row'>");
   for (var i = 0; i < fotosArray.length / 3; i += 3) {
+    document.write("<div class = 'row'>");
     for (var item in fotosArray) {
       document.write(
         "<div class='col s4'><img src='./img/" +
           fotosArray[item] +
-          "' alt='' class = 'imgGalery' onclick = 'addborder(this)'></div>"
+          "' alt='' class = 'imgGalery' onclick = 'addBorder(this)'></div>"
       );
     }
+    document.write("</div>");
   }
-  document.write("</div>");
 }
-
-function addborder(obj) {
+var arrayModif  = "";
+function addBorder(obj) {
   //obj.style.border = "1px solid red";
   //obj.className = "borderRed";
   obj.classList.add("borderRed");
+
+  /*
+  var itemAdd = obj.src.split("/");
+  var nameImg = itemAdd[itemAdd.length-1];
+  console.log(nameImg);
+  arrayModif.push(obj.src);
+*/
 }
 
 function quitarSelecciones() {
@@ -36,10 +43,12 @@ var borderRed = document.getElementsByClassName("borderRed");
 console.log(borderRed[0].src);
 //console.log(borderRed[1].src + "\n");
 
-var x = document.getElementsByTagName("img").classList; 
+var x = document.getElementsByTagName("img"); 
+console.log("Clases " + x[0].className);
+
   for (var item in borderRed){
-    console.log(x);
+    console.log("Clases " + x[item].className);
     borderRed[item].classList.remove("borderRed");
-    console.log(borderRed[item].src + "\n");
+    //console.log(borderRed[item].src + "\n");
   }
 }
