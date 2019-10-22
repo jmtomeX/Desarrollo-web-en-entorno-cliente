f0730a9dd72d3338742177e76df33c1df90ad076
jmtomeX <jmtomeX@gmail.com>
2019-10-22 13:04:19 +0200

    Añadido img

--- ejer_galeria_carrera/assets/js/main.js
+++ ejer_galeria_carrera/assets/js/main.js
@@ -1,21 +1,45 @@
-'use strict'
-
+"use strict";
 function cargarImg() {
-    console.log("CArgado");
-    var fotosArray = ["1", "4", "3", "4", "5", "6", "7", "8", "9"];
-    var cont = 0;
-    document.write("<div class = 'row'>");
-    for (var i = 0; i < fotosArray.length / 3; i += 3) {
-        console.log("Cargado for 1");
-        for (var item in fotosArray) {
-            console.log("Cargado for 2");
-            document.write("<div class='col s4'><img src='./img/" + fotosArray[item] + ".jpg' alt=''></div>");
-            console.log("item");
-        }
+  var fotosArray = [
+    "img_1.jpg",
+    "img_8.jpg",
+    "img_3.jpg",
+    "img_4.jpg",
+    "img_5.jpg",
+    "img_6.jpg",
+    "img_7.jpg",
+    "img_8.jpg",
+    "img_9.jpg"
+  ];
+  var cont = 0;
+  document.write("<div class = 'row'>");
+  for (var i = 0; i < fotosArray.length / 3; i += 3) {
+    for (var item in fotosArray) {
+      document.write(
+        "<div class='col s4'><img src='./img/" +
+          fotosArray[item] +
+          "' alt='' class = 'imgGalery' onclick = 'addborder(this)'></div>"
+      );
     }
-    document.write("</div>");
+  }
+  document.write("</div>");
+}
+
+function addborder(obj) {
+  //obj.style.border = "1px solid red";
+  //obj.className = "borderRed";
+  obj.classList.add("borderRed");
 }
 
 function quitarSelecciones() {
+var borderRed = document.getElementsByClassName("borderRed");
+console.log(borderRed[0].src);
+//console.log(borderRed[1].src + "\n");
 
+var x = document.getElementsByTagName("img").classList; 
+  for (var item in borderRed){
+    console.log(x);
+    borderRed[item].classList.remove("borderRed");
+    console.log(borderRed[item].src + "\n");
+  }
 }
