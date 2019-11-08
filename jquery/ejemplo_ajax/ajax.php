6c82d933ef5512dc67710f535b803e2ac67c0885
JOSE M TOME MEJIAS <1920OPDW107@CEBANCDEA.EDU>
2019-11-08 17:00:23 +0100

    ejemplo ajax

---
+++ jquery/ejemplo_ajax/ajax.php
@@ -0,0 +1,6 @@
+<?php 
+sleep(2);
+$file = file_get_contents("../Entorno_servidor/repaso PHP/ejer_form_table/peticiones.php"); 
+//$file = file_get_contents("https://www.google.es/"); 
+echo $file;
+?>
