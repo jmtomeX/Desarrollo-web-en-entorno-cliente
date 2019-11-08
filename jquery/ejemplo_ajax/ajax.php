30513a020ac8173f655d52c633ef7b44222b8355
JOSE M TOME MEJIAS <1920OPDW107@CEBANCDEA.EDU>
2019-11-08 19:42:42 +0100

    prueba ajax

--- jquery/ejemplo_ajax/ajax.php
+++ jquery/ejemplo_ajax/ajax.php
@@ -1,6 +1,6 @@
 <?php 
 sleep(2);
-$file = file_get_contents("../Entorno_servidor/repaso PHP/ejer_form_table/peticiones.php"); 
-//$file = file_get_contents("https://www.google.es/"); 
+//$file = file_get_contents("../Entorno_servidor/repaso PHP/ejer_form_table/peticiones.php"); 
+$file = file_get_contents("http://apuestas.jm.bymhost.com/"); 
 echo $file;
 ?>
